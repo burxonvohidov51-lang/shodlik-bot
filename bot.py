7153ddf89eefc9957aa98211283d35eda4e0b752
burxonvohidov51-lang <burxonvohidov51@gmail.com>
2026-06-24 16:53:47 +0500

    Refactor bot.py for environment variables and access control
    Refactored bot.py to use environment variables for sensitive data and added user ID checks for command access.

--- bot.py
+++ bot.py
@@ -1,12 +1,39 @@
+import os
+import requests
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8928390346:AAEVKjM8VLX0_B9tSgP0KKhAxeYp2Ndj4tY"
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+UZUM_API = os.getenv("UZUM_API_KEY")
+
+ADMIN_ID = 1487812692
+SHOP_ID = 75362
+
+headers = {
+    "Authorization": UZUM_API,
+    "Content-Type": "application/json"
+}
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Ассалому алайкум! Shodlik Bot ишга тушди.")
+    if update.effective_user.id != ADMIN_ID:
+        return
+
+    await update.message.reply_text(
+        "/products - товары\n"
+        "/price sku цена\n"
+        "/orders\n"
+        "/expenses"
+    )
+
+async def products(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id != ADMIN_ID:
+        return
+
+    await update.message.reply_text("Список товаров загружается...")
 
 app = Application.builder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("products", products))
 
 app.run_polling()
